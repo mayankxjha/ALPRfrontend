2d0580630d776f253797b0d8c71a9f84cb444e5a
Mayank Jha <mayankxjha@gmail.com>
2023-03-26 11:33:19 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {AboutComponent} from "./components/about/about.component";
 import {UploadComponent} from "./components/upload/upload.component";
 import {TheBackgroundComponent} from "./components/the-background/the-background.component";
 
-const routes: Routes = [{path:'', component:AboutComponent, children: [{
-  path:'about', component: TheBackgroundComponent}]},
-  {path:'upload', component:UploadComponent}];
+const routes: Routes = [{
+  path: '', component: AboutComponent, children: [{
+    path: '', component: TheBackgroundComponent
+  }]
+},
+  {path: 'upload', component: UploadComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
